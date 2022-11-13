7c53c8eb67edd1a29342ed104680f943c4163a6d
Väinö Pollari <vaino.pollari@tuni.fi>
2022-11-13 02:01:31 +0200

    1.5 Step 5 valmis

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,25 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -27,7 +29,7 @@ const App = () => {
 const Header = (props) => {
   return (
     <h1>
-      {props.course}
+      {props.name}
     </h1>
   )
 }
